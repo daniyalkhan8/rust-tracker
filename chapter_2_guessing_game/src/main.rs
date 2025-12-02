b6975e5c1161d5c584431a9183caa68f648c2f09
Daniyal Durrani <daniyal.durrani@alphamudigital.com>
2025-12-02 21:34:16 +0500

    Rust Book: Chapter 2 programming a guessing game completed.

---
+++ chapter_2_guessing_game/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guessing Game!");
+
+    let secret_number: u32 = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Enter your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed correct! {secret_number}");
+                break;
+            }
+        };
+    }
+}
