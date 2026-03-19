441f56d5a6f17919eb0e278029c604c3f9337837
Daniyal Durrani <daniyal.durrani@alphamudigital.com>
2026-03-19 21:23:57 +0500

    Rust Book: Updated code in Chapter 12 with Iterators and Closures in Chapter 13

--- chapter_12_minigrep/src/main.rs
+++ chapter_12_minigrep/src/main.rs
@@ -12,14 +12,23 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments.");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-            ignore_case: env::var("IGNORE_CASE").is_ok(),
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config{
+            query,
+            file_path,
+            ignore_case,
         })
     }
 }
@@ -41,8 +50,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
